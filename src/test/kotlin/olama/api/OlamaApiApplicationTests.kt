7d1dd9db64bec81acc64229e9b9d13a9e106e644
mcdod <mfkorol@mail.ru>
2024-07-01 17:49:49 +0500

    Модель чата, рефакторинт

--- src/test/kotlin/olama/api/OlamaApiApplicationTests.kt
+++ src/test/kotlin/olama/api/OlamaApiApplicationTests.kt
@@ -2,8 +2,10 @@ package olama.api
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootTest
+@ComponentScan(lazyInit = true)
 class OlamaApiApplicationTests {
 
 	@Test
